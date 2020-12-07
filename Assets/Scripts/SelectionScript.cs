627ce59932a6cc67ace35a9c6a02904a0c36cb66
Nikola <nikola19817@its.edu.rs>
2020-12-07 19:10:39 +0100

    Selekcija, Timer za  pustanje reklama, prikaz prodavnice/informacija

--- Assets/Scripts/SelectionScript.cs
+++ Assets/Scripts/SelectionScript.cs
@@ -8,159 +8,116 @@ using UnityEngine.UI;
 public class SelectionScript : MonoBehaviour
 {
     Dictionary<int, string> listaProdavnica;
+    public List<string> selektovaneProdavnice;
     public GameObject mapa;
     public GameObject Content;
     public GameObject togglePrefab;
     
-
-    // Start is called before the first frame update
     void Start()
     {
-        
         listaProdavnica = new Dictionary<int, string>();
-        /*listaProdavnica.Add(1, "Nike");
-        listaProdavnica.Add(10, "Adidas");
-        listaProdavnica.Add(15, "Puma");
-        listaProdavnica.Add(20, "Fila");
-        listaProdavnica.Add(31, "TEST 31");
-        listaProdavnica.Add(32, "TEST 32");
-        listaProdavnica.Add(33, "TEST 33");
-        listaProdavnica.Add(25, "Gigatron");
-        listaProdavnica.Add(30, "Bershka");
-        listaProdavnica.Add(38, "TEST 38");
-        listaProdavnica.Add(39, "TEST 39");
-        listaProdavnica.Add(40, "TEST 40");*/
+        selektovaneProdavnice = new List<string>();
         
-        for(int i = 0; i < 67; i++)
-        {
-            listaProdavnica.Add(i + 1, "Prodavnica " + (i + 1).ToString());
-        }
-
-        foreach(Transform t in mapa.transform)
-        {
-            t.GetComponent<Button>().onClick.AddListener(delegate { SelektujUSpisku(t.name); });
-        }
-
+        for(int i = 0; i < 67; i++) { listaProdavnica.Add(i + 1, "Prodavnica " + (i + 1).ToString()); }
+        foreach(Transform t in mapa.transform){ t.GetComponent<Button>().onClick.AddListener(delegate { SelectPrekoMape(t.name); }); }
         popuniSpisakProdavnica();
     }
-
     void popuniSpisakProdavnica()
     {
-        foreach (Transform t in Content.transform)
-        {
-            Destroy(t.gameObject);
-        }
-        for(int i = 0; i < listaProdavnica.Count; i++)
-        {
+        foreach (Transform t in Content.transform){ Destroy(t.gameObject); }
+        for(int i = 0; i < listaProdavnica.Count; i++){
             GameObject temp = Instantiate(togglePrefab, Content.transform);
             temp.GetComponent<Toggle>().isOn = false;
             temp.name = listaProdavnica.ElementAt(i).Key.ToString();
             temp.transform.Find("Label").GetComponent<Text>().text = listaProdavnica.ElementAt(i).Value;
-            temp.GetComponent<Toggle>().onValueChanged.AddListener(delegate { SelektujNaMapi(temp); });
-        }
+            temp.GetComponent<Toggle>().onValueChanged.AddListener(delegate { SelectPrekoSpiska(temp); });}
     }
-    void SelektujNaMapi(GameObject toggle)
+    void SelectPrekoSpiska(GameObject toggle)
     {
-        //Debug.Log(toggle.transform.Find("Label").GetComponent<Text>().text);
         int brProdavnice = 0;
         brProdavnice = Convert.ToInt32(toggle.name);
         if(brProdavnice != 0)
         {
             GameObject zaSelektovanje;
-            if (brProdavnice == 31 || brProdavnice == 32)
-            {
+            if (brProdavnice == 31 || brProdavnice == 32){
                 zaSelektovanje = mapa.transform.Find("31 - 32").gameObject;
-
                 GameObject tridesetJedan = Content.transform.Find("31").gameObject;
                 GameObject tridesetDva = Content.transform.Find("32").gameObject;
-                if (!tridesetJedan.GetComponent<Toggle>().isOn && !tridesetDva.GetComponent<Toggle>().isOn)
-                {
+
+                if (!tridesetJedan.GetComponent<Toggle>().isOn && !tridesetDva.GetComponent<Toggle>().isOn){
                     zaSelektovanje.GetComponent<Image>().color = new Color(243, 243, 243, 255);
-                    zaSelektovanje.GetComponent<UkljucenoIskljuceno>().istaknuto = false;
+                    if (selektovaneProdavnice.Contains("31")) selektovaneProdavnice.Remove("31");
+                    if (selektovaneProdavnice.Contains("32")) selektovaneProdavnice.Remove("32");
                 }
-                else 
-                { 
+                else{ 
                     zaSelektovanje.GetComponent<Image>().color = Color.red;
-                    zaSelektovanje.GetComponent<UkljucenoIskljuceno>().istaknuto = true;
+                    if (selektovaneProdavnice.Contains(toggle.name)) selektovaneProdavnice.Remove(toggle.name);
+                    else selektovaneProdavnice.Add(toggle.name);
                 }
 
             }
-            else if(brProdavnice == 38 || brProdavnice == 39 || brProdavnice == 40)
-            {
+            else if(brProdavnice == 38 || brProdavnice == 39 || brProdavnice == 40){
                 zaSelektovanje = mapa.transform.Find("38 - 40").gameObject;
-
                 GameObject tridesetOsam = Content.transform.Find("38").gameObject;
                 GameObject tridesetDevet = Content.transform.Find("39").gameObject;
                 GameObject cetrdeset = Content.transform.Find("40").gameObject;
-                if (!tridesetOsam.GetComponent<Toggle>().isOn && !tridesetDevet.GetComponent<Toggle>().isOn && !cetrdeset.GetComponent<Toggle>().isOn)
-                {
+
+                if (!tridesetOsam.GetComponent<Toggle>().isOn && !tridesetDevet.GetComponent<Toggle>().isOn && !cetrdeset.GetComponent<Toggle>().isOn){
                     zaSelektovanje.GetComponent<Image>().color = new Color(243, 243, 243, 255);
-                    zaSelektovanje.GetComponent<UkljucenoIskljuceno>().istaknuto = false;
+                    if (selektovaneProdavnice.Contains("38")) selektovaneProdavnice.Remove("38");
+                    if (selektovaneProdavnice.Contains("39")) selektovaneProdavnice.Remove("39");
+                    if (selektovaneProdavnice.Contains("40")) selektovaneProdavnice.Remove("40");
                 }
-                else
-                {
+                else{
                     zaSelektovanje.GetComponent<Image>().color = Color.red;
-                    zaSelektovanje.GetComponent<UkljucenoIskljuceno>().istaknuto = true;
+                    if (selektovaneProdavnice.Contains(toggle.name)) selektovaneProdavnice.Remove(toggle.name);
+                    else selektovaneProdavnice.Add(toggle.name);
                 }
             }
-            else
-            {
+            else{
                 zaSelektovanje = mapa.transform.Find(brProdavnice.ToString()).gameObject;
-
-                if (zaSelektovanje.name == "")
-                {
-                    Debug.Log("Greska! Nije pronadjena prodavnica");
-                    return;
-                }
-                else
-                {
-                    if (toggle.GetComponent<Toggle>().isOn)
-                    {
+                if (zaSelektovanje.name == ""){ Debug.Log("Greska! Nije pronadjena prodavnica"); return; }
+                else{
+                    if (toggle.GetComponent<Toggle>().isOn){
                         zaSelektovanje.GetComponent<Image>().color = Color.red;
-                        zaSelektovanje.GetComponent<UkljucenoIskljuceno>().istaknuto = true;
+                        selektovaneProdavnice.Add(toggle.name);
                     }
-                    else
-                    {
+                    else{
                         zaSelektovanje.GetComponent<Image>().color = new Color(243, 243, 243, 255);
-                        zaSelektovanje.GetComponent<UkljucenoIskljuceno>().istaknuto = false;
+                        selektovaneProdavnice.Remove(toggle.name);
                     }
                 }
             }            
         }
-        else
-        {
-            Debug.Log("Greska! Nije poslat dobar broj prodavnice");
-        }
+        else{ Debug.Log("Greska! Nije poslat dobar broj prodavnice"); }
     }
-    public void SelektujUSpisku(string brProdavnice)
+    public void SelectPrekoMape(string brProdavnice)
     {
-        if(brProdavnice != "")
-        {
-            if (brProdavnice == "31 - 32")
-            {
-                bool prodavnicaIstaknuta = mapa.transform.Find(brProdavnice).gameObject.GetComponent<UkljucenoIskljuceno>().istaknuto = !mapa.transform.Find(brProdavnice).gameObject.GetComponent<UkljucenoIskljuceno>().istaknuto;
-                Content.transform.Find("31").gameObject.GetComponent<Toggle>().isOn = prodavnicaIstaknuta;
-                Content.transform.Find("32").gameObject.GetComponent<Toggle>().isOn = prodavnicaIstaknuta;
+        if(brProdavnice != ""){
+            if (brProdavnice == "31 - 32"){
+                if(!selektovaneProdavnice.Contains("31") && !selektovaneProdavnice.Contains("32")){
+                    Content.transform.Find("31").gameObject.GetComponent<Toggle>().isOn = true;
+                    Content.transform.Find("32").gameObject.GetComponent<Toggle>().isOn = true;
+                }
+                else{
+                    Content.transform.Find("31").gameObject.GetComponent<Toggle>().isOn = false;
+                    Content.transform.Find("32").gameObject.GetComponent<Toggle>().isOn = false;
+                }
             }
-            else if(brProdavnice == "38 - 40")
-            {
-                bool prodavnicaIstaknuta = mapa.transform.Find(brProdavnice).gameObject.GetComponent<UkljucenoIskljuceno>().istaknuto = !mapa.transform.Find(brProdavnice).gameObject.GetComponent<UkljucenoIskljuceno>().istaknuto;
-                Content.transform.Find("38").gameObject.GetComponent<Toggle>().isOn = prodavnicaIstaknuta;
-                Content.transform.Find("39").gameObject.GetComponent<Toggle>().isOn = prodavnicaIstaknuta;
-                Content.transform.Find("40").gameObject.GetComponent<Toggle>().isOn = prodavnicaIstaknuta;
+            else if(brProdavnice == "38 - 40"){   
+                if(!selektovaneProdavnice.Contains("38") && !selektovaneProdavnice.Contains("39") && !selektovaneProdavnice.Contains("40")){
+                    Content.transform.Find("38").gameObject.GetComponent<Toggle>().isOn = true;
+                    Content.transform.Find("39").gameObject.GetComponent<Toggle>().isOn = true;
+                    Content.transform.Find("40").gameObject.GetComponent<Toggle>().isOn = true;
+                }
+                else{
+                    Content.transform.Find("38").gameObject.GetComponent<Toggle>().isOn = false;
+                    Content.transform.Find("39").gameObject.GetComponent<Toggle>().isOn = false;
+                    Content.transform.Find("40").gameObject.GetComponent<Toggle>().isOn = false;
+                }
             }
-            else
-            {
-                GameObject zaSelektovanje = Content.transform.Find(brProdavnice).gameObject;
-                if(zaSelektovanje.name != "")
-                    zaSelektovanje.GetComponent<Toggle>().isOn = !zaSelektovanje.GetComponent<Toggle>().isOn;
-                else
-                    Debug.Log("Greska! Nije pronadjena prodavnica!");
-            }            
+            else Content.transform.Find(brProdavnice).gameObject.GetComponent<Toggle>().isOn = !Content.transform.Find(brProdavnice).gameObject.GetComponent<Toggle>().isOn;            
         }
-        else
-            Debug.Log("Greska! Nije prosledjen broj prodavnice!");
-        
-
+        else Debug.Log("Greska! Nije prosledjen broj prodavnice!");
     }
 }
