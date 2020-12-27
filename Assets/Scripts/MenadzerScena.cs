7b62069f6d538979c680977de99ccd374dd0ecb0
Nikola19817 <nikolajovanovic594@gmail.com>
2020-12-28 00:11:56 +0100

    Prvi build

---
+++ Assets/Scripts/MenadzerScena.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenadzerScena : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+            UcitajAplikaciju();
+        if (Input.GetKey(KeyCode.Alpha2))
+            UcitajAdminScenu();
+    }
+    public void UcitajAplikaciju()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void UcitajAdminScenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
