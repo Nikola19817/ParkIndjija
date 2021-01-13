45365b903b977629f8c565e45c3546411df64009
Stefan Stankovic <Salas.ss94@gmail.com>
2021-01-13 11:49:17 +0100

    Lokalna kopija baze u json formatu

---
+++ Assets/Scripts/Classes/AllDatabaseClasses.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllDatabaseClasses 
+{
+    public List<Lokal> lokali;
+    public List<Brand> brandovi;
+    public List<BrandLokal> brand_lokal;
+    public List<Reklama> reklame;
+    public List<User> users;
+
+    public AllDatabaseClasses(List<Lokal> lokali, List<Brand> brandovi, List<BrandLokal> brand_lokal, List<Reklama> reklame, List<User> users)
+    {
+        this.lokali = lokali;
+        this.brandovi = brandovi;
+        this.brand_lokal = brand_lokal;
+        this.reklame = reklame;
+        this.users = users;
+    }
+}
---
+++ Assets/Scripts/JsonManager.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class JsonManager : MonoBehaviour
+{
+
+    AllDatabaseClasses localDatabase;
+    public string fileName = "TextDatabase.txt";
+
+
+    private void Awake()
+    {
+        DBconnection.OnEndDatabaseConnection += SaveJson;
+    }
+
+    public void SaveJson(AllDatabaseClasses allDatabase)
+    {
+
+        string json = JsonConvert.SerializeObject(allDatabase);
+        WriteToFile(fileName, json);
+
+    }
+
+    public void LoadJson()
+    {
+        string json = ReadFromFile(fileName);
+        localDatabase = JsonConvert.DeserializeObject<AllDatabaseClasses>(json);
+    }
+
+    private void WriteToFile(string fileName, string json)
+    {
+        string path = GetFilePath(fileName);
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+
+        using (StreamWriter writer = new StreamWriter(fileStream))
+        {
+            writer.Write(json);
+        }
+    }
+
+    private string ReadFromFile(string fileName)
+    {
+        string path = GetFilePath(fileName);
+        if (File.Exists(path))
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string json = reader.ReadToEnd();
+                return json;
+            }
+        }
+        else
+        {
+            Debug.Log("Local database not found");
+            return "";
+        }
+    }
+
+    private string GetFilePath(string fileName)
+    {
+        return Application.persistentDataPath + "/" + fileName;
+    }
+}
